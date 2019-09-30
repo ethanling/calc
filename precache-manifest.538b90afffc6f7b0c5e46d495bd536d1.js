436b2d0819b3ea27ff9a3e3543155df9f15595e2
Ethan <ethanling1@gmail.com>
2019-09-30 11:20:22 -0400

    Updates

---
+++ precache-manifest.538b90afffc6f7b0c5e46d495bd536d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0e29a3760d9f9936386707a5ba15ea2",
+    "url": "/calc/index.html"
+  },
+  {
+    "revision": "f5324ba2839906c0fa0d",
+    "url": "/calc/static/css/main.00652fa9.chunk.css"
+  },
+  {
+    "revision": "895a1fa33163e3ae4d98",
+    "url": "/calc/static/js/2.b8d7ab89.chunk.js"
+  },
+  {
+    "revision": "f5324ba2839906c0fa0d",
+    "url": "/calc/static/js/main.09aeef3b.chunk.js"
+  },
+  {
+    "revision": "0fb94e373603e44077dc",
+    "url": "/calc/static/js/runtime-main.d6b8c54c.js"
+  }
+]);
