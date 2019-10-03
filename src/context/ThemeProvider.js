f1cddb4c08865b4a66b058f326049078c4f7a85a
Ethan <ethanling1@gmail.com>
2019-10-03 00:12:24 -0400

    Reorganized theme styles

--- src/context/ThemeProvider.js
+++ src/context/ThemeProvider.js
@@ -1,57 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-const themes = {
-    dark: {
-        app: {
-            backgroundColor: "#ffdea6",
-            color: "#160504"
-        },
-        calc: {
-            backgroundColor: "#271F1F"
-        },
-        display: {
-            backgroundColor: "#423C3C"
-        },
-        button: {
-            backgroundColor: "#5F5959"
-        },
-        operationButton: {
-            backgroundColor: "#ff9130"
-        },
-        modifierButton: {
-            backgroundColor: "#9D9797"
-        },
-        controls: {
-            dark: "#271F1F",
-            accent: "#ff9130"
-        }
-    },
-    light: {
-        app: {
-            backgroundColor: "#374047",
-            color: "#DEE1E3"
-        },
-        calc: {
-            backgroundColor: "#97A1A7"
-        },
-        display: {
-            backgroundColor: "#50575F"
-        },
-        button: {
-            backgroundColor: "#3B4148"
-        },
-        operationButton: {
-            backgroundColor: "#7BD1A0"
-        },
-        modifierButton: {
-            backgroundColor: "#666F79"
-        },
-        controls: {
-            dark: "#97A1A7",
-            accent: "#7BD1A0"
-        }
-    }
-};
+import { themes } from '../themes/ThemeStyles';
 
 const initialState = {
     light: false,
---
+++ src/themes/ThemeStyles.js
@@ -0,0 +1,52 @@
+export const themes = {
+    dark: {
+        app: {
+            backgroundColor: "#ffdea6",
+            color: "#160504"
+        },
+        calc: {
+            backgroundColor: "#271F1F"
+        },
+        display: {
+            backgroundColor: "#423C3C"
+        },
+        button: {
+            backgroundColor: "#5F5959"
+        },
+        operationButton: {
+            backgroundColor: "#ff9130"
+        },
+        modifierButton: {
+            backgroundColor: "#9D9797"
+        },
+        controls: {
+            dark: "#271F1F",
+            accent: "#ff9130"
+        }
+    },
+    light: {
+        app: {
+            backgroundColor: "#374047",
+            color: "#DEE1E3"
+        },
+        calc: {
+            backgroundColor: "#97A1A7"
+        },
+        display: {
+            backgroundColor: "#50575F"
+        },
+        button: {
+            backgroundColor: "#3B4148"
+        },
+        operationButton: {
+            backgroundColor: "#7BD1A0"
+        },
+        modifierButton: {
+            backgroundColor: "#666F79"
+        },
+        controls: {
+            dark: "#97A1A7",
+            accent: "#7BD1A0"
+        }
+    }
+};
